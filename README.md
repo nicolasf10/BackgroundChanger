19020df5a847501daa78996d5623d1daf92c336d
Nicolas Fuchs <nicolasf10>
2020-12-08 09:40:17 -0300

    Full project

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# BackgroundChanger
-This program will use Unsplash's API to change my Mac's background image automatically using Python
+# Automatic Background Changer with Python
+This program uses [Unsplash.com API](https://unsplash.com/developers) to change my Mac's background image automatically using Python 3.9.
+
+## What does it do:
+This program fetches an HD image from Unsplash.com API and using osascript changes your Mac's picture automatically.
+
+## What do you need to run this script:
+- Apply for a developer's account in [Unsplash's API](https://unsplash.com/developers)
+- In the `full_filepath` variable, include the full filepath to the location of where `background.jpeg` will be stored.
+- To use this script make a directory in your Mac and clone this project and in terminal run `python app.py` or "build" using your favorite IDE.
+
+## Future tools that I hope to build:
+- Build an option to filter which image you want
+- Build an user interface for this script using `PyQt5` or `tkinter`
---
+++ app.py
@@ -0,0 +1,64 @@
+# app.py
+# Developer: Nicolas Fuchs
+
+#######################
+import requests
+import urllib
+import subprocess
+import json
+import shutil
+#######################
+
+# Unsplash Application Key
+api_access_key = '7c9vPplgXGgJfx4IRZisPMjHX4MCqxTxXHU2h6t978A'
+
+# Setting the filepath for this file (already inside this directory)
+filepath = 'tmp/background.jpeg'
+# Full filepath is required for osascript
+full_filepath = "/Users/nicolasfuchs/Desktop/backgroundImage/tmp/background.jpeg"
+
+# Change the Background script
+cmd = """/usr/bin/osascript<<END
+tell application "Finder"
+set desktop picture to POSIX file "%s"
+end tell
+END"""
+
+terminal_script = """osascript -e 'tell application "Finder" to set desktop picture to POSIX file "/Users/nicolasfuchs/Desktop/backgroundImage/tmp/background.jpeg"'"""
+
+
+# Using Unsplash API to grab random image from Unsplash
+url = 'https://api.unsplash.com/photos/random?client_id=' + api_access_key
+
+# requests 'GET' request for the webpage
+response = requests.get(url)
+# Stores the reponse content
+json_string = response.content
+
+# Uses JSON to load the content to 'parsed_json'
+parsed_json = json.loads(json_string)
+# 'photo' is a link to the image
+photo = parsed_json['urls']['full']
+
+# Open the url image, set stream to True, this will return the stream content.
+r = requests.get(photo, stream = True)
+
+# Check if the image was retrieved successfully
+if r.status_code == 200:
+    # Set decode_content value to True, otherwise the downloaded image file's size will be zero.
+    r.raw.decode_content = True
+    
+    # Open a local file with wb ( write binary ) permission.
+    with open(filepath,'wb') as f:
+        shutil.copyfileobj(r.raw, f)
+        
+    print('Image sucessfully Downloaded: ',filepath)
+else:
+    print('Image Couldn\'t be retreived')
+
+# Running osascript using subprocess
+try:
+	subprocess.Popen(cmd%full_filepath, shell=True)
+	subprocess.call(["killall Dock"], shell=True)
+except:
+	print("Your computer didn't allow this!")
